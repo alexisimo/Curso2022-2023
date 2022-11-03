03695b3a4375dfeab6389fd0393131c3f1d1a338
Alvaro Alonso <alonso.miguel.alvaro1@gmail.com>
2022-11-03 20:38:43 +0100

    When district selected, map updates with points inside the district

--- HandsOn/Group02/app/main.py
+++ HandsOn/Group02/app/main.py
@@ -18,6 +18,8 @@ rdfs = Namespace("http://www.w3.org/2000/01/rdf-schema#")
 xsd = Namespace("http://www.w3.org/2001/XMLSchema#")
 dct = Namespace("http://purl.org/dc/terms/")
 wdt = Namespace("http://www.wikidata.org/prop/")
+wdt_metro = Namespace("http://www.wikidata.org/prop/direct/")
+wd = Namespace("http://www.wikidata.org/entity/")
 
 """
 g.namespace_manager.bind('base', Namespace("http://smartcity.linkeddata.es/transport/"), override=False)
@@ -92,15 +94,49 @@ operators = prepareQuery('''
     initNs={"ont": ont, "owl": owl, "wdt": wdt}
 )
 
-for results in g.query(neighbourhoods, initBindings={'district': URIRef("http://smartcity.linkeddata.es/transport/data/district/14")}):
-    print(results.neigh, results.name)
+selectedFeatures = prepareQuery('''
+    SELECT ?sn ?c ?cap ?cp 
+    WHERE {
+        ?c rdfs:subClassOf* geosparql:Feature .
+        ?s a ?c .
+        ?s ont:hasNeighbourhood ?neigh .
+        ?neigh ont:hasDistrict ?district .
+        OPTIONAL{
+            ?s dct:identifier ?sn .
+            ?s ont:capacity ?cap } .
+        OPTIONAL{
+            ?s ont:id ?sn .
+            ?s ev:capacity ?cap } .
+        ?s geosparql:hasGeometry ?g .
+        ?g geosparql:asWKT ?cp
+    }
+    ''',
+    initNs={"geosparql": geosparql, "dct": dct, "rdfs": rdfs, "ont": ont, "ev": ev}
+)
+
+metro = prepareQuery('''
+    SELECT ?QNODE ?NAME (GROUP_CONCAT(?MLINENUMBER ; separator="|") as ?MLINENUMBERS) ?COORDS 
+    WHERE{
+        SERVICE <https://query.wikidata.org/sparql> {
+            ?QNODE wdt:P31 wd:Q928830 .
+            ?QNODE wdt:P16 wd:Q191987 .
+            ?QNODE wdt:P625 ?COORDS .
+            ?QNODE wdt:P81 ?MLINE .
+            ?MLINE wdt:P31 wd:Q15079663.
+            ?MLINE wdt:P1671 ?MLINENUMBER .
+            ?QNODE rdfs:label ?NAME .
+            FILTER(lang(?NAME) = "es")
+        }
+    }GROUP BY ?QNODE ?NAME ?COORDS LIMIT 1000
+    ''',
+    initNs={"wdt": wdt_metro, "wd":wd, "rdfs": rdfs}
+)
     
 #Parse data to list
 points = []
 for results in g.query(default):
     id = str(results.sn)
     
-    #type split to get only the last part of the URI
     type = str(results.c).split('/')[-1]
     if type[0] == 'b' :
         type = type.split('#')[1]
@@ -121,10 +157,21 @@ for results in g.query(default):
         color = "Others"
     # Merge point and type
     points.append([type +": "+ id] + point + [capacity] + [color])
-  
-#PRint points
-#for point in points:
-#    print(point)  
+
+#Parse metro data to points
+for results in g.query(metro):
+    id = str(results.MLINENUMBERS)
+    
+    type = str(results.NAME)
+    
+    point = results.COORDS
+    point = point = point.replace("Point(", "")
+    point = point.replace(")", "")
+    point = point.split(" ")
+    point = [float(point[1]), float(point[0])]
+    
+    points.append([type +": "+ id] + point + ["None"] + ["Metro"])
+    
     
 #Points to dict
 points_dict = {
@@ -161,38 +208,42 @@ from dash import dcc, html, Output, Input
 #Create dic from districts
 district = []
 for results in g.query(districts):
-    district.append([results.name])
+    district.append([str(results.name)]+ [str(results.district)])
     
 district_dict = {
-    "district" : [i[0] for i in district]
+    "district" : [i[0] for i in district],
+    "uri" : [i[1] for i in district]
 }
+    
+disf = pd.DataFrame(district_dict)
 
 #Create dic from neighbourhoods
 neighbourhood = []
 for results in g.query(neighbourhoods):
-    neighbourhood.append([results.name.title()])
+    neighbourhood.append([str(results.name).title]+ [str(results.neigh)])
     
 neighbourhood_dict = {
-    "neighbourhood" : [i[0] for i in neighbourhood]
+    "neighbourhood" : [i[0] for i in neighbourhood],
+    "uri" : [i[1] for i in neighbourhood]
 }
-    
 
-disf = pd.DataFrame(district_dict)
 neighf = pd.DataFrame(neighbourhood_dict)
 
+#-----APP-----
 app = dash.Dash()
 app.layout = html.Div([
     html.Div([
     dcc.Dropdown(
         disf['district'].unique(),
-        'Districts',
+        '',
         id='district-dropdown'
         ),],
     style={'width': '49%', 'display': 'inline-block'}),
     html.Div([
     dcc.Dropdown(
-        neighf['neighbourhood'].unique(),
+        [],
         'Neighbourhoods',
+        placeholder="Select a district",
         id='neighbourhood-dropdown'
         ),],
     style={'width': '49%', 'float': 'right', 'display': 'inline-block'}),
@@ -203,4 +254,80 @@ app.layout = html.Div([
     )
 ])
 
-app.run_server() 
+@app.callback(
+    Output('MapPlot', 'figure'),
+    Output('neighbourhood-dropdown', 'options'),
+    Input('district-dropdown', 'value'))
+def display_district(value):    
+    if(value == None):
+        return fig, []
+    
+    if(len(value) < 1):
+        return fig, []
+    
+    uri_name= disf[disf["district"] == value]['uri'].values[0]
+    
+    neighbourhood_dis= []
+    for results in g.query(neighbourhoods, initBindings={'district': URIRef(uri_name)}):
+        neighbourhood_dis.append([str(results.name).title] + [str(results.neigh)])
+
+    neighbourhood_dict_dis = {
+        "neighbourhood" : [i[0] for i in neighbourhood_dis],
+        "uri" : [i[1] for i in neighbourhood_dis]
+    }
+    neigh_dis = pd.DataFrame(neighbourhood_dict_dis)
+    
+    dis_points = []
+    for results in g.query(selectedFeatures, initBindings={'district': URIRef(uri_name)}):
+        id_dis = str(results.sn)
+        
+        type_dis = str(results.c).split('/')[-1]
+        if type_dis[0] == 'b' :
+            type_dis = type_dis.split('#')[1]
+        
+        point = results.cp
+        point = point.replace("POINT(", "")
+        point = point.replace(")", "")
+        point = point.split(" ")
+        point = [float(i) for i in point]
+        
+        capacity = str(results.cap)
+        
+        if(type_dis == "EstacionBicicleta"):
+            color = "Bike Station"
+        elif(type_dis == "EVChargingStation"):
+            color = "Charging Station"
+        else :
+            color = "Others"
+        # Merge point and type
+        dis_points.append([type_dis +": "+ id_dis] + point + [capacity] + [color])
+    
+        points_dict_dis = {
+        "name": [i[0] for i in dis_points],
+        "Latitude": [i[1] for i in dis_points],
+        "Longitude": [i[2] for i in dis_points],
+        "Capacity": [i[3] for i in dis_points],
+        "Type": [i[4] for i in dis_points]
+    }
+
+    fig_d = px.scatter_mapbox(points_dict_dis, 
+                            lat="Latitude", 
+                            lon="Longitude", 
+                            hover_name="name", 
+                            hover_data=["Capacity"],
+                            color = "Type",
+                            height=800
+                            )
+
+    fig_d.update_layout(
+        mapbox=dict(
+            center=dict(lat=40.41831, lon= -3.70275),
+            accesstoken=mapbox_access_token,
+            zoom=12,
+        ),
+        mapbox_style="mapbox://styles/rcn41/cl9y1jy8s003g14plahpakd40"
+    )
+    
+    return fig_d, neigh_dis['neighbourhood'].unique()
+
+app.run_server()
