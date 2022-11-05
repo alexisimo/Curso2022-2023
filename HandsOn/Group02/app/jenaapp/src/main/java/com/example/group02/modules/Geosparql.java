2721b9eb750ca8d53ded67df11afab6287151007
CASAMAYOR NAVAS, RAUL <raul.casamayor.navas@alumnos.upm.es>
2022-11-05 19:42:59 +0100

    Changed geosparql queries

--- HandsOn/Group02/app/jenaapp/src/main/java/com/example/group02/modules/Geosparql.java
+++ HandsOn/Group02/app/jenaapp/src/main/java/com/example/group02/modules/Geosparql.java
@@ -12,6 +12,7 @@ import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.riot.RDFDataMgr;
 
+import com.example.group02.types.TransportFeature;
 
 public class Geosparql {
     Model model;
@@ -24,25 +25,73 @@ public class Geosparql {
         SpatialIndex.buildSpatialIndex(ds);
     }
     
-    public List<String> getClosestFeatures(String resource, int maxDistance){
+    public List<TransportFeature> getClosestFeatures(String resource, int maxDistance){
         String queryLocal =  "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
                         "PREFIX spatial: <http://jena.apache.org/spatial#>" +
                         "PREFIX geo: <http://www.opengis.net/ont/geosparql#>" +
                         "PREFIX units: <http://www.opengis.net/def/uom/OGC/1.0/>"+ 
-                        "SELECT ?uris "+
+                        "PREFIX ont: <http://smartcity.linkeddata.es/transport/ontology/>"+ 
+                        "SELECT ?uris ?name "+
                         "WHERE {"+
                         "    " + resource + " geo:hasGeometry ?geo." +
                         "    ?geo geo:asWKT ?g ." +
                         "    ?c rdfs:subClassOf* geo:Feature ." +
                         "    ?uris a ?c ." +
-                        "    ?uris spatial:nearbyGeom(?g " + maxDistance + " units:meter 10)" +
+                        "    ?uris spatial:nearbyGeom(?g " + maxDistance + " units:meter) ." +
+                        "    ?uris ont:hasAddress ?ad ." +
+                        "    ?ad ont:ad_name ?adname ." +
+                        "    ?ad ont:ad_number ?adnumber ." +
+                        "    BIND(CONCAT(STR( ?adname ), \" \", STR( ?adnumber )) AS ?name )" +
                         "}";
-        List<String> res = new ArrayList<>();
+        List<TransportFeature> res = new ArrayList<>();
         try (QueryExecution qe = QueryExecution.create(queryLocal, ds)) {
             ResultSet rs = qe.execSelect();
             while(rs.hasNext()){
                 QuerySolution s = rs.next();
-                res.add(s.get("uris").toString());
+                res.add(new TransportFeature(s.get("uris").toString(), s.get("name").toString()));
+            }
+        }
+        return res;
+    }
+
+    public List<TransportFeature> getClosestMetroStations(String resource, int maxDistance){
+        String queryLocal =  "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
+                        "PREFIX spatial: <http://jena.apache.org/spatial#>" +
+                        "PREFIX geo: <http://www.opengis.net/ont/geosparql#>" +
+                        "PREFIX units: <http://www.opengis.net/def/uom/OGC/1.0/>"+ 
+                        "PREFIX ont: <http://smartcity.linkeddata.es/transport/ontology/>"+ 
+                        "PREFIX wd: <http://www.wikidata.org/entity/>"+ 
+                        "PREFIX wdt: <http://www.wikidata.org/prop/direct/>"+ 
+                        "PREFIX p: <http://www.wikidata.org/prop/>"+ 
+                        "PREFIX psv: <http://www.wikidata.org/prop/statement/value/>"+ 
+                        "PREFIX wikibase: <http://wikiba.se/ontology#>"+ 
+                        "SELECT ?name "+
+                        "WHERE {"+
+                        "    {" +
+                        "    SELECT DISTINCT ?lat ?lon ?name "+
+                        "    WHERE {" +
+                        "           SERVICE <https://query.wikidata.org/sparql> {" +
+                        "               ?QNODE wdt:P31 wd:Q928830 ." +
+                        "               ?QNODE wdt:P16 wd:Q191987 ." +
+                        "               ?QNODE p:P625 ?COORDS ." +
+                        "               ?COORDS psv:P625 ?COORDSN ." +
+                        "               ?COORDSN wikibase:geoLongitude ?lat . " +
+                        "               ?COORDSN wikibase:geoLatitude ?lon . " +
+                        "               ?QNODE wdt:P81 ?MLINE ." +
+                        "               ?MLINE wdt:P31 wd:Q15079663." +
+                        "               ?QNODE rdfs:label ?name ." +
+                        "               FILTER(lang(?name) = \"es\")" +
+                        "           }" +
+                        "    }" +
+                        "    }" +
+                        "    " + resource +" spatial:nearby(?lat ?lon " + maxDistance + " units:meter) ." +
+                        "}";
+        List<TransportFeature> res = new ArrayList<>();
+        try (QueryExecution qe = QueryExecution.create(queryLocal, ds)) {
+            ResultSet rs = qe.execSelect();
+            while(rs.hasNext()){
+                QuerySolution s = rs.next();
+                res.add(new TransportFeature("a/a/a/a/a", s.get("name").toString()));
             }
         }
         return res;
